9cd6ed2ec92a7f931dd1201aae2f9bb831be5d8a
ShubhamBharde <shubhambharde98@gmail.com>
2023-12-12 20:27:36 +0530

    add: hello world from javascript

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const root = document.querySelector("#root");
+const h1 = document.createElement("h1")
+h1.innerHTML = "Hello World from Javascript"
+
+root.appendChild(h1) // appendChild() will insert/add child in parent but not will replace 
