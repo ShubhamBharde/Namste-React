651d72d661e308e3f2211227b919c4611cd4b7c0
ShubhamBharde <shubhambharde98@gmail.com>
2023-12-12 21:39:23 +0530

    add: nested react element

--- js/App.js
+++ js/App.js
@@ -1,7 +1,17 @@
-// console.log(React, ReactDOM) // React is just object not a new thing.. 🚀 do you know that in js everything is object. 
+// console.log(React, ReactDOM) // React is just object not a new thing.. 🚀 do you know that in js everything is object.
 
-const root = ReactDOM.createRoot(document.getElementById("root")) // react-dom root
-const h1 = React.createElement("h1", {}, "Hello World from react")
+const root = ReactDOM.createRoot(document.getElementById("root")); // react-dom root
+const nestedElement = [
+  React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("h1", { id: "child" }, "Heading 1")
+  ),
+  React.createElement(
+    "div",
+    { id: "uncle" },
+    React.createElement("h2", { id: "cousin" }, "Heading 2")
+  ),
+];
 
-
-root.render(h1) 
+root.render(nestedElement);
