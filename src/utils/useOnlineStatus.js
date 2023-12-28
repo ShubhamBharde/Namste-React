b3db330b055933b9e6620c5b583138a60bbffd3e
ShubhamBharde <shubhambharde098@gmail.com>
2023-12-28 20:48:01 +0530

    custom hook: useOnlineStatus()

---
+++ src/utils/useOnlineStatus.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const useOnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("offline", () => {
+      setOnlineStatus(false);
+    });
+
+    window.addEventListener("online", () => {
+      setOnlineStatus(true);
+    });
+  }, onlineStatus);
+
+  return onlineStatus;
+};
+
+export default useOnlineStatus;
